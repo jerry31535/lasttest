975092d0f7212929b8d3b9fe73e324de93a8de88
jerry31535 <jerry61204104@gmail.com>
2025-03-02 12:56:12 +0800

    初次提交

---
+++ src/main/java/com/example/myweb/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.myweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // 會對應到 resources/templates/index.html
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about"; // 會對應到 resources/templates/about.html
+    }
+
+    @GetMapping("/team")
+    public String team() {
+        return "team"; // 會對應到 resources/templates/team.html
+    }
+    @GetMapping("game")
+    public String game() {
+        return "game"; // 對應 resources/templates/game.html
+    }
+}
